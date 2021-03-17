a3939741265569dfd5ce4561eee91f24d3f60b96
Vid <vidhatanand@gmail.com>
2021-03-17 05:34:42 +0530

    initial fixes

---
+++ order.php
@@ -0,0 +1,26 @@
+<?php
+$data = file_get_contents("php://input");
+
+$currentTimeinSeconds = time(); 
+
+if ($data == '::LONG::') {
+    $configs[$currentTimeinSeconds] = '1';
+
+} elseif ($data == '::LONG::') {
+    $configs[$currentTimeinSeconds] = '-1';
+}
+
+$configs_object = json_encode($configs);
+file_put_contents('./log.json', $configs_object);
+
+// include dirname(__FILE__)."/kiteconnect.php";
+// $configs_object = file_get_contents('./config.json');
+// $configs = json_decode($configs_object, true);
+
+// $kite = new KiteConnect($configs["api_key"]);
+// $kite->setAccessToken($configs["access_token"]);
+// foreach ($events as $event) {
+//   // Here, you now have each event and can process them how you like
+//   process_event($event);
+// }
+?>
